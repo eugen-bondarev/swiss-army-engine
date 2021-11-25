3581217ef1736fe084fc209274a01295b3d2818f
eugen-bondarev <eug.bondarev@gmail.com>
2021-11-25 22:47:13 +0100

    [vk] formatting

--- Source/VKMain.cpp
+++ Source/VKMain.cpp
@@ -180,8 +180,8 @@ int main()
 
             static float theta{0}; theta += 0.025f;
             
-            glm::mat4 pre = glm::mat4(1); pre[1][1] = -1.0f;
-            sceneUniformBuffer().proj = pre * glm::perspective(glm::radians(70.0f), window->GetAspectRatio(), 0.1f, 1000.0f); 
+            sceneUniformBuffer().proj = glm::perspective(glm::radians(70.0f), window->GetAspectRatio(), 0.1f, 1000.0f); 
+            sceneUniformBuffer().proj[1][1] *= -1.0f;
 
             for (size_t i = 0; i < meshes.size(); ++i)
             {
